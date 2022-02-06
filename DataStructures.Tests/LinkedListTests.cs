5c7e5c66cc99d521d8bd1806d15e1ef044264f87
Bash! <sheik.m.basha@outlook.com>
2022-02-06 21:43:28 +0530

    LinkedList Implementation

---
+++ DataStructures.Tests/LinkedListTests.cs
@@ -0,0 +1,184 @@
+using DataStructures.LinkedList;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class LinkedListTests
+    {
+        [Fact]
+        public void ListIsEmptyInitially()
+        {
+            // Arrange
+            MyLinkedList<int> linkedList = new();
+
+            // Act, Assert
+            Assert.True(linkedList.IsEmpty());
+        }
+
+        [Fact]
+        public void ListIsNotEmptyAfterAdditionOperation()
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtBeginning(5);
+            linkedList.AddNodeAtBeginning(10);
+
+            // Act, Assert.
+            Assert.False(linkedList.IsEmpty());
+
+        }
+
+        [Theory]
+        [MemberData(nameof(TestTraverseData))]
+        public void TraverseList(int value1, int value2, int value3, List<int> expectedResult)
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtEnd(value1);
+            linkedList.AddNodeAtEnd(value2);
+            linkedList.AddNodeAtEnd(value3);
+
+            // Act
+            var actualResult = linkedList.Traverse();
+
+            // Assert.
+            Assert.Equal(expectedResult, actualResult);
+
+        }
+
+        [Fact]
+        public void AddNodeAtBeginning()
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtBeginning(5);
+            linkedList.AddNodeAtBeginning(10);
+            var expectedResult = new List<int>() { 10, 5 };
+
+            // Act
+            var actualResult = linkedList.Traverse();
+
+            // Assert.
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void AddNodeAtEnd()
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtBeginning(5);
+            linkedList.AddNodeAtBeginning(10);
+            linkedList.AddNodeAtEnd(100);
+            var expectedResult = new List<int>() { 10, 5 , 100};
+
+            // Act
+            var actualResult = linkedList.Traverse();
+
+            // Assert.
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData(2,9)]
+        public void AddNodeAtIndex(int index, int value)
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtBeginning(5);
+            linkedList.AddNodeAtBeginning(10);
+            linkedList.AddNodeAtEnd(100);
+            linkedList.AddNodeAtIndex(value, index);
+            var expectedResult = new List<int>() { 10, 9, 5, 100 };
+
+            // Act
+            var actualResult = linkedList.Traverse();
+
+            // Assert.
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void DeleteFirstNode()
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtBeginning(5);
+            linkedList.AddNodeAtBeginning(10);
+            linkedList.AddNodeAtBeginning(15);
+            var expectedResult = new List<int>() { 10, 5 };
+
+
+            // Act
+            linkedList.DeleteFirstNode();
+            var actualResult = linkedList.Traverse();
+
+            // Assert.
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void DeleteLastNode()
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtBeginning(5);
+            linkedList.AddNodeAtBeginning(10);
+            linkedList.AddNodeAtBeginning(15);
+            var expectedResult = new List<int>() {15, 10};
+
+
+            // Act
+            linkedList.DeleteLastNode();
+            var actualResult = linkedList.Traverse();
+
+            // Assert.
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void DeleteNodeAtIndex()
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtBeginning(5);
+            linkedList.AddNodeAtBeginning(10);
+            linkedList.AddNodeAtBeginning(15);
+            var expectedResult = new List<int>() { 15, 5 };
+
+
+            // Act
+            linkedList.DeleteNodeAtIndex(2);
+            var actualResult = linkedList.Traverse();
+
+            // Assert.
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+
+        [Theory]
+        [InlineData(10, true)]
+        [InlineData(200, false)]
+        public void ElementExists(int value, bool expectedResult)
+        {
+            // Arrange.
+            MyLinkedList<int> linkedList = new();
+            linkedList.AddNodeAtBeginning(5);
+            linkedList.AddNodeAtBeginning(10);
+            linkedList.AddNodeAtBeginning(15);
+
+            // Act
+            var actualResult = linkedList.Exists(value);
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+        public static IEnumerable<object[]> TestTraverseData =>
+            new List<object[]>
+            {
+            new object[] { 5, 10, 15, new List<int> { 5, 10, 15 } },
+            };
+
+
+    }
+}
---
+++ DataStructures/LinkedList/LinkedList.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+
+        public Node<T>? Next { get; set; }
+
+        public Node(T data)
+        {
+            Value = data;
+            Next = null;
+        }
+
+        public Node()
+        {
+            Value = default;
+            Next = null;
+        }
+    }
+
+    public class MyLinkedList<T>
+    {
+        private Node<T> head = new();
+
+        public MyLinkedList()
+        {
+        }
+
+        #region "Utility"
+        public bool IsEmpty() => head.Next == null;
+
+        public int Count()
+        {
+            int listLength = 0;
+            var temp = head;
+
+            while (temp?.Next != null)
+            {
+                listLength++;
+                temp = temp.Next;
+            }
+            return listLength;
+        }
+        #endregion
+
+        #region "Insertion"
+
+        public void AddNodeAtBeginning(T data)
+        {
+            Node<T> newNode = new(data);
+
+            if(IsEmpty())
+            {
+                head.Next = newNode;
+            }
+            else
+            {
+                newNode.Next = head.Next;
+                head.Next = newNode;
+            }
+        }
+        public void AddNodeAtEnd(T data)
+        {
+            Node<T> newNode = new(data);
+
+            if (IsEmpty())
+            {
+                head.Next = newNode;
+            }
+            else
+            {
+                var temp = head.Next;
+                while(temp?.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = newNode;
+            }
+        }
+
+        public void AddNodeAtIndex(T data, int index)
+        {
+            if(index < 0)
+            {
+                throw new ArgumentOutOfRangeException("index should be non-negative value");
+            }
+
+            if(index > Count())
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+
+            var temp = head;
+            int currentListLength = 0;
+
+            while(temp.Next != null && currentListLength+1 < index)
+            {
+                temp = temp?.Next;
+                currentListLength++;
+            }
+
+            Node<T> newNode = new Node<T>(data);
+            newNode.Next = temp.Next;
+            temp.Next = newNode;
+            
+        }
+        #endregion
+
+        #region "Deletion"
+        public void DeleteFirstNode()
+        {
+            if(IsEmpty())
+            {
+                throw new Exception("List is Empty");
+            }
+
+            var temp = head.Next;
+            head.Next = temp.Next;
+
+            // instead we can also use the below
+            // head.Next = head.Next.Next;
+        }
+
+        public void DeleteLastNode()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("List is Empty");
+            }
+
+            // If list has only 1 element.
+            if(Count() == 1)
+            {
+                head.Next = null;
+            }
+
+            var temp = head.Next;
+            while(temp.Next != null && temp.Next.Next != null)
+            {
+                temp = temp?.Next;
+            }
+
+            temp.Next = null;
+        }
+
+        public void DeleteNodeAtIndex(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("index should be non-negative value");
+            }
+
+            if (index > Count())
+            {
+                throw new ArgumentOutOfRangeException("index");
+            }
+
+            var temp = head;
+            int currentListLength = 0;
+
+            while (temp.Next != null && currentListLength + 1 < index)
+            {
+                temp = temp?.Next;
+                currentListLength++;
+            }
+
+            temp.Next = temp.Next.Next;
+        }
+        #endregion
+
+        #region "Traversing & Searching"
+        public bool Exists(T data)
+        {
+            if (!IsEmpty())
+            {
+                var temp = head.Next;
+                do
+                {
+                    if (temp.Value.Equals(data))
+                    {
+                        return true;
+                    }
+                    temp = temp.Next;
+                }
+                while (temp != null);
+
+            }
+            return false;
+        }
+
+        public List<T> Traverse()
+        {
+            List<T> result = new();
+            if (!IsEmpty())
+            {
+                var temp = head?.Next;
+                
+
+                do
+                {
+                    result.Add(temp.Value);
+                    temp = temp.Next;
+                }
+                while (temp != null);
+            }
+            return result;
+        }
+        #endregion
+
+        #region "Reverse"
+
+        public void ReverseWithIteration()
+        {
+
+        }
+
+        public void ReverseWithRecursion()
+        {
+
+        }
+        #endregion
+    }
+}
